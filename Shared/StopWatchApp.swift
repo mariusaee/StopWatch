c836edd236f04efaca569bd769f38f23b7a119bb
Marius <mariusae@me.com>
2022-04-08 14:09:35 +0300

    Initial Commit

---
+++ Shared/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  Shared
+//
+//  Created by Marius Malyshev on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
